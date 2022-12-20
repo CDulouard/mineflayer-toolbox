ec899508450c047d49bc09ae85b5608690beadea
CDulouard <clement.dulouard@gmail.com>
2022-12-20 23:45:18 +0100

    Add movement speed functions

--- src/move/move.ts
+++ src/move/move.ts
@@ -1,33 +1,48 @@
-import {Bot} from "mineflayer";
+import { Bot } from 'mineflayer'
 
 export const forward = (bot: Bot | any) => {
-    stop(bot)
-    bot.setControlState('forward', true)
+  stop(bot)
+  bot.setControlState('forward', true)
 }
 
 export const backward = (bot: Bot | any) => {
-    stop(bot)
-    bot.setControlState('back', true)
+  stop(bot)
+  bot.setControlState('back', true)
 }
 
 export const left = (bot: Bot | any) => {
-    stop(bot)
-    bot.setControlState('left', true)
+  stop(bot)
+  bot.setControlState('left', true)
 }
 
 export const right = (bot: Bot | any) => {
-    stop(bot)
-    bot.setControlState('right', true)
+  stop(bot)
+  bot.setControlState('right', true)
 }
 
 export const jump = (bot: Bot | any) => {
-    bot.setControlState('jump', true)
-    bot.setControlState('jump', false)
+  bot.setControlState('jump', true)
+  bot.setControlState('jump', false)
+}
+
+export const sneak = (bot: Bot | any) => {
+  bot.setControlState('sprint', false)
+  bot.setControlState('sneak', true)
+}
+
+export const walk = (bot: Bot | any) => {
+  bot.setControlState('sprint', false)
+  bot.setControlState('sneak', false)
+}
+
+export const sprint = (bot: Bot | any) => {
+  bot.setControlState('sneak', false)
+  bot.setControlState('sprint', true)
 }
 
 export const stop = (bot: Bot | any) => {
-    bot.setControlState('forward', false)
-    bot.setControlState('back', false)
-    bot.setControlState('left', false)
-    bot.setControlState('right', false)
+  bot.setControlState('forward', false)
+  bot.setControlState('back', false)
+  bot.setControlState('left', false)
+  bot.setControlState('right', false)
 }
